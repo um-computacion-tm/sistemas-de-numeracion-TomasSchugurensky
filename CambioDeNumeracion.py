5f5c25c3163ca46b0919e30e4fc35c06d90a96ff
Tomas Schugurensky <tomas@pop-os.localdomain>
2023-04-24 22:38:55 -0300

    agregar mas sistemas

--- CambioDeNumeracion.py
+++ CambioDeNumeracion.py
@@ -1,11 +1,77 @@
+#Conversiones ordinarias
+def decimal2binario(decimal):
+    binario = 0
+    while decimal >= 1:
+        binario = str(decimal%2) + binario
+        decimal //= 2
+    return binario
+
+def decimal2octal(decimal):
+    octal = []
+    while decimal > 0:
+        res = decimal%8
+        octal.append(str(res))
+        octal = octal [::-1]
+    return octal
+
+def decimal2hexadecimal(decimal):
+    hexa = []
+    while decimal > 0:
+        res = decimal%16
+        if res <= 9:
+            hexa.append(str(res))
+        elif res == esp:
+            esp = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E',15:'F'}
+            hexa.append(str(res+esp))
+            hexa = hexa [::-1]
+    return hexa
 
 def binario2decimal(binario):
-    binario = '01011100'
-    decimal = binario.split.pow() // -1
-    return decimal
-def decimal2binario(decimal):
-    decimal = 97
-    binario = decimal % 2
-    return binario
-    
+    decimal = 0
+    pos = len(binario)
+    for i in range (pos):
+        if binario [pos - i -1] == '1':
+            decimal +=2**i
+        return decimal
 
+def octal2decimal(octal):
+    decimal = 0
+    pos = 0
+    for numero in octal[::-1]:
+        decimal += int(numero)*(8**pos)
+        pos *=1
+        return decimal
+
+def hexa2decimal(hexa):
+    decimal = 0
+    pos = 0
+    for numero in hexa[::-1]:
+        valor = int(numero, 16)
+        decimal += valor * (16**pos)
+        pos *=1
+        return decimal
+
+#Conversiones no directas
+def binario2octal(binario):
+    binario2decimal
+    decimal2octal
+    
+def binario2hexa(binario):
+    binario2decimal
+    decimal2hexadecimal
+
+def octal2binario(octal):
+    octal2decimal
+    decimal2binario
+
+def octal2hexa(octal):
+    octal2decimal
+    decimal2hexadecimal
+
+def hexa2binario(hexa):
+    hexa2decimal
+    decimal2binario
+
+def hexa2octal(hexa):
+    hexa2decimal
+    decimal2octal
--- TestNumeracion.py
+++ TestNumeracion.py
@@ -1,12 +1,40 @@
 import unittest
-from CambioDeNumeracion import binario2decimal
 from CambioDeNumeracion import decimal2binario
+from CambioDeNumeracion import decimal2hexadecimal
+from CambioDeNumeracion import decimal2octal
+from CambioDeNumeracion import binario2decimal
+from CambioDeNumeracion import octal2decimal
+from CambioDeNumeracion import hexa2decimal
+from CambioDeNumeracion import binario2octal
+from CambioDeNumeracion import binario2hexa
+from CambioDeNumeracion import octal2binario
+from CambioDeNumeracion import octal2hexa
+from CambioDeNumeracion import hexa2binario
+from CambioDeNumeracion import hexa2octal
 class TestNumeracion(unittest.TestCase):
     def test_binario2decimal(self):
         self.assertEqual(binario2decimal('01011100'), 96)
-        
     def test_decimal2binario(self):
         self.assertEqual(decimal2binario(97), '01100001')
-
+    def test_decimal2hexadecimal(self):
+        self.assertEqual(decimal2hexadecimal(249), 'F9')
+    def test_decimal2octal(self):
+        self.assertEqual(decimal2octal(98), 142)
+    def test_octal2decimal(self):
+        self.assertEqual(octal2decimal(143), 99)
+    def test_hexa2decimal(self):
+        self.assertEqual(hexa2decimal('FA'), 250)
+    def test_binario2octal(self):
+        self.assertEqual(binario2octal('00010111011'), 273)
+    def test_binario2hexa(self):
+        self.assertEqual(binario2hexa('00010111011'), 'BB')
+    def test_octal2binario(self):
+        self.assertEqual(octal2binario(100), ' 1000000 ')
+    def test_octal2hexa(self):
+        self.assertEqual(octal2hexa(100), 40)
+    def test_hexa2binario(self):
+        self.assertEqual(hexa2binario(101), '100000001')
+    def test_hexa2octal(self):
+        self.assertEqual(hexa2octal(101), 401)
 if __name__ =="__main__":
     unittest.main
